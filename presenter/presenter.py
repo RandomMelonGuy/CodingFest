81722bcbf67c788ed0de27f710abc9e2f970842d
Melon <arbuzrar@gmail.com>
2025-11-27 16:16:46 +0500

    interupdate presenter change

--- presenter/presenter.py
+++ presenter/presenter.py
@@ -2,11 +2,22 @@ from view.view import TestView
 from model.model import Model
 
 class TestPresenter:
-    def __init__(self):
-        self.model = Model()
+    def __init__(self, working_file):
+        self.model = Model(working_file)
         self.view = TestView(self)
         self.view.mainloop()
-
-    def get_random(self, maxVal: int) -> int:
-        return self.model.get_random(maxVal)
     
+    def upload_file(self, filepath: str) -> str:
+        responce = self.model.setup(filepath)
+        return responce
+    
+    def print_data(self):
+        responce = self.model.print_data()
+        return responce
+    
+    def add_row(self, data):
+        responce = self.model.add_row(data)
+        return responce
+    
+    def save(self):
+        self.model.save()
